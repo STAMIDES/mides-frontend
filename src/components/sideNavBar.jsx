f96db541e0bcb2eb896955a29eced3f6217123d2
Rodri <ranitacafetera@gmail.com>
2024-05-15 19:39:26 -0300

    add links to sidenav

--- src/components/sideNavBar.jsx
+++ src/components/sideNavBar.jsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { Box, IconButton, Typography, Avatar } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import { Box, Typography, Avatar } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
 
 const SideNavBar = () => {
   const navigate = useNavigate();
 
   const handleLogoClick = () => {
-    navigate('/');
+    navigate("/");
   };
+
+  const menuItems = [
+    { label: "Pedidos", path: "/requests" },
+    { label: "Planificaciones", path: "/planning" },
+    { label: "Usuarios", path: "/users" },
+    { label: "Camionetas", path: "/vans" }
+  ];
 
   return (
     <Box
@@ -24,31 +31,29 @@ const SideNavBar = () => {
         sx={{ width: 56, height: 56, cursor: "pointer", mb: 4, alignSelf: "center"}}
         onClick={handleLogoClick}
       />
-      {[
-       "Pedidos", 
-       "Planificaciones", 
-       "Usuarios", 
-       "Camionetas"
-    ].map((elem) => (
-        <IconButton
-        key={elem}
-        size="large"
-        color="default"
-        sx={{
-          color: 'white',
-          mb: 2,
-          borderRadius: '4px', // Set a border radius for rounded corners
-          transition: 'background-color 0.3s',
-          '&:hover': {
-            backgroundColor: 'rgba(255, 255, 255, 0.2)',
-          },
-          '&:active': {
-            backgroundColor: 'rgba(255, 255, 255, 0.4)', // Darker when clicked
-          },
-        }}
-      >
-          <Typography variant="h6">{elem}</Typography>
-        </IconButton>
+      {menuItems.map((item) => (
+        <Link key={item.label} to={item.path} style={{ textDecoration: 'none', color: 'inherit', width: '100%' }}>
+          <Box
+            key={item.label}
+            textAlign="left"
+            py={1}
+            px={2}
+            mb={2}
+            color="white"
+            borderRadius={1}
+            transition="background-color 0.3s"
+            sx={{
+              "&:hover": {
+                backgroundColor: "rgba(255, 255, 255, 0.1)"
+              },
+              "&:active": {
+                backgroundColor: "rgba(255, 255, 255, 0.2)"
+              }
+            }}
+          >
+            <Typography variant="h6">{item.label}</Typography>
+          </Box>
+        </Link>
       ))}
     </Box>
   );
