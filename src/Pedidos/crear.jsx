e1b19aa7653843270199bebe3d390a5a5e951847
Rodri <ranitacafetera@gmail.com>
2024-05-20 01:03:34 -0300

    start pedidos create

--- src/Pedidos/crear.jsx
+++ src/Pedidos/crear.jsx
@@ -1,24 +1,26 @@
 import React, { useState } from 'react';
-import { Box, TextField, Button, Typography, Select, MenuItem } from '@mui/material';
+import { Box, TextField, Button, Typography, FormControlLabel, Select, MenuItem, Checkbox } from '@mui/material';
 import api from '../network/axios';
 
-// Definición del Enum TipoCliente 
-const TipoCliente = {
+// Definición del Enum TipoPedido 
+const TipoPedido = {
   particular: 'Usuario particular',
   dispositivo: 'Usuario de dispositivo del MIDES',
   salud: 'Centro de salud o rehabilitación'
 };
 
-const CrearCliente = ({ cliente = {} }) => {
+const CrearPedido = ({ pedido = {} }) => {
   const [formData, setFormData] = useState({
-    documento: '',
     nombre: '',
     apellido: '',
-    telefono: '',
-    email: '',
+    documento: '',
+    direccion_origen: '',
+    ventana_destino_fin: '',
+    direccion_destino: '',
+    prioridad: '',
+    acompañante: '',
     observaciones: '',
-    tipo: '',
-    ...cliente
+    ...pedido
   });
 
   const [errors, setErrors] = useState({});
@@ -28,6 +30,16 @@ const CrearCliente = ({ cliente = {} }) => {
     setFormData({ ...formData, [name]: value });
   };
 
+  const handleChangeDoc = (e) => {
+    const { name, value } = e.target;
+    const clientes = api.get(`clientes/doc/${value}`);
+  };
+  
+  const handleChangeNombreoApellido = (e) => {
+    const { name, value } = e.target;
+    const clientes = api.get(`clientes/nombre/${value}`);
+  };
+  
   const validarDoc = () => {
     let tempErrors = {};
     if (!formData.documento) {
@@ -38,17 +50,11 @@ const CrearCliente = ({ cliente = {} }) => {
     setErrors(tempErrors);
     return Object.keys(tempErrors).length === 0;
   };
-  const validarTel = () => {
-    let tempErrors = {};
-    if (isNaN(formData.telefono )) {
-      tempErrors.telefono = 'Telefono debe ser un número';
-    }
-    setErrors(tempErrors);
-    return Object.keys(tempErrors).length === 0;
-  };
+
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!validarDoc() || !validarTel()) {
+    if (!validarDoc()) {
       return;
     }
 
@@ -59,24 +65,24 @@ const CrearCliente = ({ cliente = {} }) => {
     };
 
     try {
-      const response = await api.post('clientes', dataToSubmit);
-      console.log('Nuevo cliente agregado:', response.data);
+      const response = await api.post('pedidos', dataToSubmit);
+      console.log('Nuevo pedido agregado:', response.data);
       // Opcionalmente redirigir a otra página tras enviar el formulario correctamente
-      // history.push('/clientes'); // Suponiendo que tienes acceso al objeto history
+      // history.push('/pedidos'); // Suponiendo que tienes acceso al objeto history
     } catch (error) {
-      console.error('Error al agregar cliente:', error);
+      console.error('Error al agregar pedido:', error);
     }
   };
 
   return (
     <Box>
-      <Typography variant="h4" gutterBottom>Agregar Nuevo Cliente</Typography>
+      <Typography variant="h4" gutterBottom>Agregar Nuevo Pedido</Typography>
       <form onSubmit={handleSubmit}>
         <TextField
           name="nombre"
           label="Nombre"
           value={formData.nombre}
-          onChange={handleChange}
+          onChange={handleChangeNombreoApellido}
           fullWidth
           required
         />
@@ -84,7 +90,7 @@ const CrearCliente = ({ cliente = {} }) => {
           name="apellido"
           label="Apellido"
           value={formData.apellido}
-          onChange={handleChange}
+          onChange={handleChangeNombreoApellido}
           fullWidth
           required
         />
@@ -92,56 +98,64 @@ const CrearCliente = ({ cliente = {} }) => {
           name="documento"
           label="Documento"
           value={formData.documento}
-          onChange={handleChange}
+          onChange={handleChangeDoc}
           fullWidth
           required
           error={!!errors.documento}
           helperText={errors.documento}
         />
         <TextField
-          name="telefono"
-          label="Teléfono"
-          value={formData.telefono}
+          name="direccion_origen"
+          label="Dirección de Origen"
+          value={formData.direccion_origen}
           onChange={handleChange}
           fullWidth
         />
         <TextField
-          name="email"
-          label="Email"
-          value={formData.email}
+          name="direccion_destino"
+          label="Dirección de Destino"
+          value={formData.direccion_destino}
           onChange={handleChange}
           fullWidth
         />
         <TextField
+          name="ventana_destino_fin"
+          label="Hora de llegada al destino"
+          value={formData.ventana_destino_fin}
+          onChange={handleChange}
+          fullWidth
+        />
+        <FormControlLabel
+          name="prioridad"
+          control={<Checkbox defaultChecked />} 
+          label="Prioridad"
+          value={formData.prioridad}
+          onChange={handleChange}
+          required
+        />
+          <FormControlLabel
+          name="acompañante"
+          control={<Checkbox defaultChecked />} 
+          label="Lleva acompañante"
+          value={formData.acompañante}
+          onChange={handleChange}
+          required
+        />
+          <TextField
           name="observaciones"
           label="Observaciones"
           value={formData.observaciones}
           onChange={handleChange}
-          fullWidth
-        />
-        <Select
-          name="tipo"
-          value={formData.tipo}
-          onChange={handleChange}
           displayEmpty
           fullWidth
           required
-        >
-          <MenuItem value="" disabled>
-            Seleccionar Tipo de Cliente
-          </MenuItem>
-          {Object.entries(TipoCliente).map(([key, value]) => (
-            <MenuItem key={key} value={key}>
-              {value}
-            </MenuItem>
-          ))}
-        </Select>
+        />
         <Button type="submit" variant="contained" color="primary" style={{ marginTop: '1rem' }}>
-          Agregar Cliente
+          Agregar Pedido
         </Button>
       </form>
     </Box>
   );
 };
 
-export default CrearCliente;
+export default CrearPedido;
