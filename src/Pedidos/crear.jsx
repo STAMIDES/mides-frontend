f9dbedb533276fed43825ffe7959deaa8045fcac
Rodri <ranitacafetera@gmail.com>
2024-05-20 01:23:56 -0300

    add autocomplete, missing the fill all the field when selected

--- src/Pedidos/crear.jsx
+++ src/Pedidos/crear.jsx
@@ -1,13 +1,6 @@
-import React, { useState } from 'react';
-import { Box, TextField, Button, Typography, FormControlLabel, Select, MenuItem, Checkbox } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { Box, TextField, Button, Typography, FormControlLabel, Checkbox, Autocomplete } from '@mui/material';
 import api from '../network/axios';
-
-// Definición del Enum TipoPedido 
-const TipoPedido = {
-  particular: 'Usuario particular',
-  dispositivo: 'Usuario de dispositivo del MIDES',
-  salud: 'Centro de salud o rehabilitación'
-};
 
 const CrearPedido = ({ pedido = {} }) => {
   const [formData, setFormData] = useState({
@@ -24,33 +17,30 @@ const CrearPedido = ({ pedido = {} }) => {
   });
 
   const [errors, setErrors] = useState({});
+  const [documentOptions, setDocumentOptions] = useState([]);
+  const [nameOptions, setNameOptions] = useState([]);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleChangeDoc = (e) => {
-    const { name, value } = e.target;
-    const clientes = api.get(`clientes/doc/${value}`);
-  };
-  
-  const handleChangeNombreoApellido = (e) => {
-    const { name, value } = e.target;
-    const clientes = api.get(`clientes/nombre/${value}`);
-  };
-  
-  const validarDoc = () => {
-    let tempErrors = {};
-    if (!formData.documento) {
-      tempErrors.documento = 'Documento es requerido';
-    } else if (isNaN(formData.documento)) {
-      tempErrors.documento = 'Documento debe ser un número';
+  const handleChange = (e, value, reason) => {
+    if (reason === 'selectOption') {
+      setFormData({ ...formData, ...value });
+    } else {
+      const { name, value } = e.target;
+      setFormData({ ...formData, [name]: value });
     }
-    setErrors(tempErrors);
-    return Object.keys(tempErrors).length === 0;
   };
 
+  const fetchDocuments = async (input) => {
+    if (input.length > 2) {
+      try {
+        const response = await api.get(`clientes/doc/${input}`);
+        const clientesDoc = response.data.clientes.map((cliente) => `${cliente.documento}`);
+        console.log(clientesDoc)
+        setDocumentOptions(clientesDoc);
+      } catch (error) {
+        console.error('Error fetching documents:', error);
+      }
+    }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -72,39 +62,66 @@ const CrearPedido = ({ pedido = {} }) => {
     } catch (error) {
       console.error('Error al agregar pedido:', error);
     }
+  }
+  const fetchNames = async (input) => {
+    if (input.length > 2) {
+      try {
+        const response = await api.get(`clientes/nombre/${input}`);
+        const clienteNombres = response.data.clientes.map((cliente) => `${cliente.nombre} ${cliente.apellido}`);
+        setNameOptions(clienteNombres);
+      } catch (error) {
+        console.error('Error fetching names:', error);
+      }
+    }
+  };
+
+  const handleInputDocumentChange = (event, value) => {
+    fetchDocuments(value);
+  };
+
+  const handleInputNameChange = (event, value) => {
+    fetchNames(value);
   };
 
   return (
     <Box>
       <Typography variant="h4" gutterBottom>Agregar Nuevo Pedido</Typography>
       <form onSubmit={handleSubmit}>
-        <TextField
-          name="nombre"
-          label="Nombre"
-          value={formData.nombre}
-          onChange={handleChangeNombreoApellido}
-          fullWidth
-          required
+        <Autocomplete
+          freeSolo
+          options={nameOptions}
+          getOptionLabel={(option) => option}
+          onInputChange={handleInputNameChange}
+          onChange={(event, value) => handleChange(event, value, 'selectOption')}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              name="nombre"
+              label="Nombre"
+              fullWidth
+              required
+            />
+          )}
         />
-        <TextField
-          name="apellido"
-          label="Apellido"
-          value={formData.apellido}
-          onChange={handleChangeNombreoApellido}
-          fullWidth
-          required
+        <Autocomplete
+          freeSolo
+          options={documentOptions}
+          getOptionLabel={(option) => option}
+          onInputChange={handleInputDocumentChange}
+          onChange={(event, value) => handleChange(event, value, 'selectOption')}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              name="documento"
+              label="Documento"
+              fullWidth
+              required
+              error={!!errors.documento}
+              helperText={errors.documento}
+            />
+          )}
         />
-        <TextField
-          name="documento"
-          label="Documento"
-          value={formData.documento}
-          onChange={handleChangeDoc}
-          fullWidth
-          required
-          error={!!errors.documento}
-          helperText={errors.documento}
-        />
-        <TextField
+         <TextField
           name="direccion_origen"
           label="Dirección de Origen"
           value={formData.direccion_origen}
