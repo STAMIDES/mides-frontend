cff751dd781c068012fa2d1ec051893ba4543a18
Rodri <ranitacafetera@gmail.com>
2024-06-09 23:45:51 -0300

    listado rename

--- src/Clientes/index.jsx
+++ src/Clientes/index.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import ClienteListado from './Listado';
+import ClienteListado from './listado';
 import ClienteCrear from './crear';
 
 const ClientesRutas = () => {
