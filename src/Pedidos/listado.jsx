bcf47dca09f63b5b1f62c187fb7ee99122d18be0
criptoc <ranitacafetera@gmail.com>
2025-03-16 17:33:16 -0300

    arreglar busqueda de listado de pedidos

--- src/Pedidos/listado.jsx
+++ src/Pedidos/listado.jsx
@@ -11,7 +11,9 @@ import moment from 'moment';
 const columns = [
   { label: "Solicitado por", key: "nombre_y_apellido" },
   { label: "Documento", key: "usuario_documento" },
+  { label: "Tipo", key: "tipo" },
   { label: "Origen", key: "direccion_origen_y_horario"},
+  { label: "Paradas Intermedias", key: "paradas_intermedias"},
   { label: "Destino", key: "direccion_destino_y_horario" },
 ]
 
@@ -58,34 +60,26 @@ const PedidoListado = () => {
     pedidosSinProcesar.sort((a, b) => new Date(a.fecha_programado) - new Date(b.fecha_programado));
     pedidosSinProcesar.forEach(pedido => {
         const nombreYApellido = `${pedido.cliente.nombre}\n${pedido.cliente.apellido}`;
-        const usuario_documento = pedido.cliente.documento;
         const paradas = pedido.paradas;
         paradas.sort((a, b) => a.posicion_en_pedido - b.posicion_en_pedido);
-        var lastDestino
-        for (let i = 0; i + 1 < paradas.length; i += 1) {
-            const origen = paradas[i];
-            const destino = paradas[i + 1];
-            
-            var direccionOrigenYHorario;
-            var direccionDestinoYHorario;
-            if (origen===lastDestino){
-              direccionOrigenYHorario = `${origen.direccion} \n ${origen.ventana_horaria_fin || 'Sin horario'}`;
-            }else{
-              direccionOrigenYHorario = `${origen.direccion} \n ${origen.ventana_horaria_inicio  || 'Sin horario'}`;
-            }
-            direccionDestinoYHorario = `${destino.direccion} \n ${destino.ventana_horaria_inicio || 'Sin horario'}`;
-            nuevoListado.push({
-                id: pedido.id,
-                nombre_y_apellido: nombreYApellido,
-                usuario_documento: usuario_documento,
-                direccion_origen_y_horario: direccionOrigenYHorario,
-                direccion_destino_y_horario: direccionDestinoYHorario,
-                estado: pedido.estado
-            });
-            lastDestino = destino;
+        var direccionOrigenYHorario = `${paradas[0].direccion} \n ${paradas[0].ventana_horaria_inicio || 'Sin horario'}`;
+        var cantParadas = paradas.length;
+        var direccionDestinoYHorario = `${paradas[cantParadas-1].direccion} \n ${paradas[cantParadas-1].ventana_horaria_inicio || 'Sin horario'}`;
+        var paradasIntermedias = '';
+        for (let i = 1; i +1 < cantParadas ; i += 1) {
+            paradasIntermedias += '*' + paradas[i].direccion + '\n Llegada: ' + paradas[i].ventana_horaria_inicio + '\n Salida: ' + paradas[i].ventana_horaria_fin + '\n\n' ;
         }
+        nuevoListado.push({
+            id: pedido.id,
+            tipo: pedido.tipo,
+            nombre_y_apellido: nombreYApellido,
+            usuario_documento: pedido.cliente_documento,
+            direccion_origen_y_horario: direccionOrigenYHorario,
+            paradas_intermedias: paradasIntermedias,
+            direccion_destino_y_horario: direccionDestinoYHorario
+        });
     });
-    
+    console.log(nuevoListado);
     return nuevoListado;
   }
 
@@ -101,6 +95,7 @@ const PedidoListado = () => {
       if (error.response) {
         setError(error.response.statusText);
       } else {
+        console.trace(error);
         setError('An unexpected error occurred');
       }
     }
@@ -109,9 +104,12 @@ const PedidoListado = () => {
   const handleDateChange = (newDate) => {
     if (moment(newDate, "DD-MM-YYYY", true).isValid()) {
       setDate(moment(newDate, "YYYY-MM-DD"));
+      setPage(1);
     }
   };
   const handleSearch = (searchTerm) => {
+    console.log(searchTerm);
+    setPage(1);
     setSearchTerm(searchTerm);
   };
 
@@ -128,7 +126,7 @@ const PedidoListado = () => {
         icons={[ <DeleteIcon />]}
         iconsTooltip={[ "Eliminar Solicitud"]}
         onDelete={handleDelete}
-        getFunction={obtenerPedidos}
+        getFunction={setPage}
         currentPage={currentPage}
         pageCounter={Math.ceil(cantidadPedidos/pageSize)}
       />
