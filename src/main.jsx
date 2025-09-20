25602d5e44d29901a454ba3eec0ab6fb166f05b2
Juan Pablo Rubio <juan.rubio@streaver.com>
2025-09-20 00:40:49 -0300

    fix icons

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx';
 import "./index.css"
+import L from 'leaflet'
+
+// Override default Leaflet marker icons to use files from public/imgs
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: '/imgs/marker-icon-2x.png',
+  iconUrl: '/imgs/marker-icon.png',
+  shadowUrl: '/imgs/marker-shadow.png',
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
